cd2f977e1c5cf3b921dd034b31e0b212d0bf1d1e
Allan Joe <54856207+ourpalal@users.noreply.github.com>
2020-11-21 20:15:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Node-Svc. Simple microservice. Includes both Express and Fetch. Calls itself, 
-// or can be replicated and will round-robin requests among peers.  x
+// or can be replicated and will round-robin requests among peers.
 
 'use strict';
 
